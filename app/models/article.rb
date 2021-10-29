4cd31d0dfb07232c4493ae1c736df25ac7ec89eb
Soumyadeep Sen <soumyadeep.sen@anchanto.com>
2021-10-29 11:33:15 +0530

    A Lot of changes...

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
     belongs_to :user
-    validates :title, presence: true, length: {minimum:3, maximum:100}
-    validates :description, presence: true, length: {minimum:3, maximum:200}
-end
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  end
